311aeb5021b93f0778a33f35e1eaa31e681653f2
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-07 19:30:38 +0900

    ファイル名変更

--- src/main/java/com/diworksdev/login/util/DBConnector.java
+++ src/main/java/com/diworksdev/login/util/DBConnector.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBCConnector {
+public class DBConnector {
 	
 	private static String driverName = "com.mysql.jdbc.Driver";
 	private static String url = "jdbc:mysql://localhost/logindb_yabu";
